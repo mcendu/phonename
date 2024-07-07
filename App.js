2244c1eab766cf3bcbb0517b7270eecde0797099
Nathan Du <nathandu@outlook.com>
2024-07-07 19:55:06 +0800

    basic name card

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { OrientationLock } from 'expo-screen-orientation';
 
 export default function App() {
+  ScreenOrientation.lockAsync(OrientationLock.LANDSCAPE_LEFT);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.welcome}>Hello I am</Text>
+      <View style={styles.nameBox}>
+        <Text style={styles.name}>Nathan</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#110a01',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: '#f0f0f0',
+    textAlign: 'center',
+  },
+  nameBox: {
+    backgroundColor: '#f09910',
+    paddingHorizontal: 24,
+    borderRadius: 24,
+  },
+  name: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: '#110a01',
+    textAlign: 'center',
   },
 });
